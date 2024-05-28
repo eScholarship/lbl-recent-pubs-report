574725d0dfa9b9fa356012fc4cb0721831a1e0bc
Devin <devin.smith.work@gmail.com>
2024-05-28 11:49:43 -0700

    Added subprocess for email

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import creds
 import elements_db_functions
 import csv
 from datetime import datetime
+import subprocess
 
 
 def main():
@@ -9,13 +10,23 @@ def main():
 
     new_lbl_pub_records = elements_db_functions.get_new_lbl_pub_records(sql_creds)
 
-    filename = "LBL-90-Day-pub-records-" + datetime.today().strftime('%Y-%m-%d')
+    filename = "LBL-90-Day-pub-records-" + datetime.today().strftime('%Y-%m-%d') + ".csv"
 
     with open("output/" + filename, "w") as outfile:
         csv_writer = csv.writer(outfile)
         csv_writer.writerow(new_lbl_pub_records[0].keys())
         for row in new_lbl_pub_records:
             csv_writer.writerow(row.values())
+
+    subprocess.run(
+        [
+            'mail',
+            '-s', 'New DOE-funded pub records w/o eSchol deposits',
+            '-a', 'output/' + filename,
+            creds.email_recipient
+         ],
+        capture_output=True
+    )
 
 
 # Stub for main
